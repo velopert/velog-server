746eb05194ce268b4b4ca27ec8750f0c24e8c865
velopert <public.velopert@gmail.com>
2019-02-09 00:06:26 +0900

    Implements local register

--- src/routes/api/v2/auth/auth.ts
+++ src/routes/api/v2/auth/auth.ts
@@ -8,19 +8,23 @@ import EmailAuth from '../../../../entity/EmailAuth';
 import shortid = require('shortid');
 import { createAuthEmail } from '../../../../etc/emailTemplates';
 import sendMail from '../../../../lib/sendMail';
-import { generateToken } from '../../../../lib/token';
+import { generateToken, decodeToken } from '../../../../lib/token';
+import { decode } from 'punycode';
+import UserProfile from '../../../../entity/UserProfile';
 
 const auth = new Router();
 
 /* LOCAL AUTH */
 
-/*
-  POST /api/v2/auth/send-auth-email
-  {
-    email: "public.velopert@gmail.com"
-  }
-*/
-auth.post('/send-auth-email', async ctx => {
+/**
+ * Send Auth Email
+ *
+ * POST /api/v2/auth/sendmail
+ * {
+ *  email: "public.velopert@gmail.com"
+ * }
+ */
+auth.post('/sendmail', async ctx => {
   type RequestBody = {
     email: string;
   };
@@ -44,20 +48,27 @@ auth.post('/send-auth-email', async ctx => {
     emailAuth.email = email;
     await getRepository(EmailAuth).save(emailAuth);
     const emailTemplate = createAuthEmail(!!user, emailAuth.code);
-
-    // send email
-    await sendMail({
-      to: email,
-      ...emailTemplate,
-      from: 'verify@velog.io'
-    });
     ctx.body = {
       registered: !!user
     };
+
+    setImmediate(() => {
+      sendMail({
+        to: email,
+        ...emailTemplate,
+        from: 'verify@velog.io'
+      });
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
 });
+
+/**
+ * Get Register Token or Login with Email
+ *
+ * GET /api/v2/auth/code/:code
+ */
 auth.get('/code/:code', async ctx => {
   const { code }: { code: string } = ctx.params;
   try {
@@ -72,7 +83,7 @@ auth.get('/code/:code', async ctx => {
 
     // check date
     const diff = new Date().getTime() - new Date(emailAuth.created_at).getTime();
-    if (diff > 1000 * 60 * 60 * 24) {
+    if (diff > 1000 * 60 * 60 * 24 || emailAuth.logged) {
       ctx.status = 410;
       ctx.body = {
         name: 'EXPIRED_CODE'
@@ -102,10 +113,129 @@ auth.get('/code/:code', async ctx => {
     } else {
       // generate user token
     }
-  } catch (e) {}
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 });
-auth.post('/code-login', async ctx => {});
-auth.post('/register/local', async ctx => {});
+
+/**
+ * Local Email Register
+ *
+ * POST /api/v2/auth/register/local
+ * {
+ * 	 "register_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZlbG9nLmlvLmFwcEBnbWFpbC5jb20iLCJpZCI6IjcxMWIyYTBkLTc3YTAtNDQxZS1hOTQzLTQ0Zjk0YjljZjE0YiIsImlhdCI6MTU0OTYzNDk2MywiZXhwIjoxNTQ5NjM4NTYzLCJpc3MiOiJ2ZWxvZy5pbyIsInN1YiI6ImVtYWlsLXJlZ2lzdGVyIn0.5OwnQRhbThR3tk8ak_CjhZfPMizkNt3pP6rUFQZjecM",
+ * 	 "form": {
+ *  		"display_name": "Hello",
+ *  		"username": "helloworldsd",
+ *  		"short_bio": ""
+ *  	}
+ * }
+ */
+auth.post('/register/local', async ctx => {
+  type RequestBody = {
+    register_token: string;
+    form: {
+      display_name: string;
+      username: string;
+      short_bio: string;
+    };
+  };
+
+  type RegisterToken = {
+    email: string;
+    id: string;
+    sub: string;
+  };
+
+  const schema = Joi.object().keys({
+    register_token: Joi.string().required(),
+    form: Joi.object()
+      .keys({
+        display_name: Joi.string()
+          .min(1)
+          .max(45)
+          .required(),
+        username: Joi.string()
+          .regex(/^[a-z0-9-_]+$/)
+          .min(3)
+          .max(16)
+          .required(),
+        short_bio: Joi.string()
+          .allow('')
+          .max(140)
+      })
+      .required()
+  });
+
+  if (!validateBody(ctx, schema)) return;
+
+  const {
+    register_token,
+    form: { username, short_bio, display_name }
+  }: RequestBody = ctx.request.body;
+
+  // check token
+  let decoded: RegisterToken | null = null;
+  try {
+    decoded = await decodeToken<RegisterToken>(register_token);
+    if (decoded.sub !== 'email-register') {
+      ctx.status = 400;
+      ctx.body = {
+        name: 'INVALID_TOKEN'
+      };
+      return;
+    }
+  } catch (e) {
+    ctx.body = {
+      name: 'INVALID_TOKEN'
+    };
+    return;
+  }
+
+  // check duplicates
+  const { email, id: codeId } = decoded;
+  const exists = await getRepository(User)
+    .createQueryBuilder()
+    .where('email = :email OR username = :username', { email, username })
+    .getOne();
+  if (exists) {
+    ctx.status = 409;
+    ctx.body = {
+      name: 'ALREADY_EXISTS',
+      payload: email === exists.email ? 'email' : 'username'
+    };
+    return;
+  }
+
+  // disable code
+  const emailAuthRepo = getRepository(EmailAuth);
+  const emailAuth = await emailAuthRepo.findOne(codeId);
+  if (emailAuth) {
+    emailAuth.logged = true;
+    await emailAuthRepo.save(emailAuth);
+  }
+
+  // create user
+  const userRepo = getRepository(User);
+  const user = new User();
+  user.email = email;
+  user.is_certified = true;
+  user.username = username;
+  await userRepo.save(user);
+
+  const profile = new UserProfile();
+  profile.user = user;
+  profile.display_name = display_name;
+  profile.short_bio = short_bio;
+  await getRepository(UserProfile).save(profile);
+  const profileJSON = { ...profile };
+  delete profileJSON.user;
+  delete profileJSON.fk_user_id;
+  ctx.body = {
+    ...user,
+    profile: profileJSON
+  };
+});
 
 /* SOCIAL AUTH */
 auth.use('/social', social.routes());
