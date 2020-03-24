102aa8452644a4974c201c20de767675521521df
velopert <public.velopert@gmail.com>
2020-03-24 23:17:48 +0900

    Remove trailing dots in url slug - close #26

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -48,12 +48,13 @@ export const validateBody = (ctx: Context, schema: SchemaLike) => {
 export const escapeForUrl = (text: string): string => {
   return text
     .replace(
-      /[^0-9a-zA-Zㄱ-힣\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf -]/g,
+      /[^0-9a-zA-Zㄱ-힣.\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf -]/g,
       ''
     )
     .trim()
     .replace(/ /g, '-')
-    .replace(/--+/g, '-');
+    .replace(/--+/g, '-')
+    .replace(/\.+$/, '');
 };
 
 export function checkEmpty(text: string) {
