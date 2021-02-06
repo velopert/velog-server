14a3bb996d7c69764dcf8344daaed6e8bd4e2be8
velopert <public.velopert@gmail.com>
2021-02-07 03:24:08 +0900

    Update hard-coded spam filter

--- src/etc/spamFilter.ts
+++ src/etc/spamFilter.ts
@@ -1,6 +1,33 @@
 // hard coded spam filter
 export default function spamFilter(text: string) {
-  const replaced = text.replace(/[^a-zA-Zㄱ-힣0-9]/g, '').toLowerCase();
+  let replaced = text
+    .replace(/[^a-zA-Zㄱ-힣0-9\n]/g, '')
+    .replace(/\n\s*\n/g, '\n')
+    .toLowerCase();
+
+  const linksAtEnd = text
+    .replace(/\n\s*\n/g, '\n')
+    .toLowerCase()
+    .split('\n')
+    .reverse()
+    .reduce((acc, current, index, arr) => {
+      if (/^http/.test(current) && (index === 0 || /^http/.test(arr[index - 1]))) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+
+  const noKorean = !/[ㄱ-힣]/g.test(text);
+
+  if (linksAtEnd > 4 && noKorean) {
+    return true;
+  }
+
+  if (replaced.includes('watch') && replaced.includes('free') && noKorean) return true;
+  if (replaced.includes('besflix')) return true;
+  if (replaced.includes('dumpspanda')) return true;
+  if (replaced.includes('dump') && noKorean) return true;
+
   if (replaced.includes('mfd8') && replaced.includes('카톡')) {
     return true;
   }
@@ -20,7 +47,8 @@ export default function spamFilter(text: string) {
     (replaced.includes('reddit') && replaced.includes('live')) ||
     (replaced.includes('deviantart') && replaced.includes('live')) ||
     replaced.includes('allsportslives') ||
-    (replaced.includes('arsenal') && replaced.includes('stream'))
+    (replaced.includes('arsenal') && replaced.includes('stream')) ||
+    replaced.includes('handballonline')
   )
     return true;
   return false;
