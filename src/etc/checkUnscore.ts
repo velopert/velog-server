a1103011dfb53c9f23e0b3286f42f27c9e854b71
velopert <public.velopert@gmail.com>
2021-09-01 00:51:38 +0900

    Remove space from text

--- src/etc/checkUnscore.ts
+++ src/etc/checkUnscore.ts
@@ -2,7 +2,7 @@ const unscoredCategory = (process.env.UNSCORED_CATEGORY || '').split(',');
 const unscoredWords = (process.env.UNSCORED_WORDS || '').split(',');
 
 export default function checkUnscore(text: string) {
-  const lowerText = text.toLowerCase();
+  const lowerText = text.toLowerCase().replace(/ /g, '');
   const isUnscoredCategory = unscoredCategory.some(category => lowerText.includes(category));
   const hasUnscoredWords = unscoredWords.some(word => lowerText.includes(word));
   return isUnscoredCategory && hasUnscoredWords;
