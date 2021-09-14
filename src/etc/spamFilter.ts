72a2d16847aee5c3dae7825412440f9ad35f3f5c
velopert <public.velopert@gmail.com>
2021-09-15 02:57:59 +0900

    Update spam filter

--- src/etc/spamFilter.ts
+++ src/etc/spamFilter.ts
@@ -1,3 +1,6 @@
+const bannedKeywords = (process.env.BANNED_KEYWORDS || '').split(',');
+const bannedAltKeywords = (process.env.BANNED_ALT_KEYWORDS || '').split(',');
+
 // hard coded spam filter
 export default function spamFilter(text: string) {
   let replaced = text
@@ -23,64 +26,11 @@ export default function spamFilter(text: string) {
     return true;
   }
 
-  if (replaced.includes('watch') && replaced.includes('free') && noKorean) return true;
-  if (replaced.includes('besflix')) return true;
-  if (replaced.includes('dumpspanda')) return true;
-  if (replaced.includes('dump') && noKorean) return true;
-  if (replaced.includes('instructurecom')) return true;
-
-  if (replaced.includes('mfd8') && replaced.includes('카톡')) {
+  if (bannedKeywords.some(keyword => replaced.includes(keyword))) {
     return true;
   }
-  if (replaced.includes('ghtk') && replaced.includes('카톡')) {
-    return true;
-  }
-  if (replaced.includes('낙태')) return true;
-  if (replaced.includes('dumpsarchive')) return true;
-  if (replaced.includes('dumpsvision')) return true;
-  if (
-    (replaced.includes('barcelona') && replaced.includes('reddit')) ||
-    replaced.includes('pelicansvclippers')
-  )
-    return true;
-  if (
-    replaced.includes('wwwgametv') ||
-    (replaced.includes('reddit') && replaced.includes('live')) ||
-    (replaced.includes('deviantart') && replaced.includes('live')) ||
-    replaced.includes('allsportslives') ||
-    (replaced.includes('arsenal') && replaced.includes('stream')) ||
-    replaced.includes('handballonline')
-  )
-    return true;
 
-  const bannedKeywords = [
-    'electrum',
-    'coinomi',
-    'reset password',
-    'mycelium',
-    '8103554365',
-    'support phone',
-    'customer support',
-    'customer care number',
-    'support phone service',
-    'account recovery',
-    'find password',
-    'reset password',
-    '3554365',
-    ':phone:',
-    'helpline',
-    'airlines reservations',
-    'casino',
-    '온카지노',
-    'erectile',
-    'dysfunction',
-    'cenforce',
-    'micasonnam',
-  ];
-
-  const keywords = ['airline', 'flight', 'booking', 'customer', 'center', 'how to', 'helpline'];
-
-  const score = keywords.reduce((acc, current) => {
+  const score = bannedAltKeywords.reduce((acc, current) => {
     if (replaced.includes(current)) {
       return acc + 1;
     }
@@ -88,9 +38,6 @@ export default function spamFilter(text: string) {
   }, 0);
 
   if (score >= 2) return true;
-  if (bannedKeywords.some(keyword => replaced.includes(keyword))) {
-    return true;
-  }
 
   return false;
 }
