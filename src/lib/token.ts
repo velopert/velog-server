6e42faf6cf132de50b28eae7c555a1a0944f1838
velopert <public.velopert@gmail.com>
2022-03-06 03:28:39 +0900

    fix: :bug: Specifies domains to set cookie

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -47,7 +47,7 @@ export const decodeToken = async <T = any>(token: string): Promise<T> => {
   });
 };
 
-const domains = ['.velog.io', undefined];
+const domains = ['.velog.io', undefined, 'v2cdn.velog.io', 'v2.velog.io'];
 export function setTokenCookie(
   ctx: Context,
   tokens: { accessToken: string; refreshToken: string }
