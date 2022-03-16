6b426bc136bc45212b3d5e7cc8bd1bc1d54e527e
velopert <public.velopert@gmail.com>
2022-03-17 02:17:48 +0900

    feat: parses graphCDN addr

--- src/lib/middlewares/ipaddr.ts
+++ src/lib/middlewares/ipaddr.ts
@@ -1,6 +1,7 @@
 import { Middleware } from 'koa';
 
 export const ipaddr: Middleware = (ctx, next) => {
-  ctx.state.ipaddr = ctx.request.ips.slice(-1)[0] || ctx.request.ip;
+  const graphCdnAddr = ctx.request.headers['gcdn-client-ip'];
+  ctx.state.ipaddr = graphCdnAddr || ctx.request.ips.slice(-1)[0] || ctx.request.ip;
   return next();
 };
