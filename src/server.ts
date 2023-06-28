7f76d5db1a742447fdc0c7be7f44256579148ddb
carrick <carrick@chafgames.com>
2023-06-28 15:16:49 +0900

    Add: console.log for server infomation when stared

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import Database from './database';
 import { startClosing } from './lib/middlewares/keepAlive';
 import http from 'http';
 
-const { PORT } = process.env;
+const { PORT, DATABASE_URL } = process.env;
 
 const database = new Database();
 database.getConnection().then(database => {
   app.listen(PORT, () => {
     process.send?.('ready');
+    console.info(`Server is listening on ${PORT}`);
+    console.info(`Database is connected URL: ${DATABASE_URL}`);
   });
 
   process.on('SIGINT', function () {
