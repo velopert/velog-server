17e33f786ecfe01c8c74848b665c5e1ac23a8473
velopert <public.velopert@gmail.com>
2024-01-04 14:56:48 +0900

    fix: temporarily disable sitemap. to be implemented from v3

--- src/routes/sitemaps/sitemaps.ctrl.ts
+++ src/routes/sitemaps/sitemaps.ctrl.ts
@@ -43,30 +43,38 @@ ${link.priority ? `<priority>${link.priority}</priority>` : ''}
 }
 
 export const sitemapIndex: Middleware = async ctx => {
-  const months = listAllMonths();
-
-  const postCount = await getRepository(Post).count({
-    where: {
-      is_temp: false,
-      is_private: false,
-    },
-  });
-
-  const pages = Math.floor(postCount / 2500);
-  const array = new Array(pages).fill(0).map((_, i) => i + 1);
-
-  const sitemaps = array
-    .map(page => `https://velog.io/sitemaps/posts-${page}.xml`)
-    .concat('https://velog.io/sitemaps/general.xml')
-    .map(location => `<sitemap><loc>${location}</loc></sitemap>`)
-    .join('');
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${sitemaps}
 </sitemapindex>`;
 
   ctx.set('Content-Type', 'text/xml');
   ctx.body = xml;
+
+  // temporarily disable sitemap
+  //   const months = listAllMonths();
+
+  //   const postCount = await getRepository(Post).count({
+  //     where: {
+  //       is_temp: false,
+  //       is_private: false,
+  //     },
+  //   });
+
+  //   const pages = Math.floor(postCount / 2500);
+  //   const array = new Array(pages).fill(0).map((_, i) => i + 1);
+
+  //   const sitemaps = array
+  //     .map(page => `https://velog.io/sitemaps/posts-${page}.xml`)
+  //     .concat('https://velog.io/sitemaps/general.xml')
+  //     .map(location => `<sitemap><loc>${location}</loc></sitemap>`)
+  //     .join('');
+  //   const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  // <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  // ${sitemaps}
+  // </sitemapindex>`;
+
+  //   ctx.set('Content-Type', 'text/xml');
+  //   ctx.body = xml;
 };
 
 export const generalSitemap: Middleware = ctx => {
@@ -93,31 +101,31 @@ export const generalSitemap: Middleware = ctx => {
 };
 
 export const postsSitemap: Middleware = async ctx => {
-  ctx.set('Content-Type', 'text/xml');
-  const page = parseInt(ctx.params.page, 10);
-
-  // ctx.body = { firstDay, lastDay };
-  try {
-    const posts = await getRepository(Post)
-      .createQueryBuilder('post')
-      .select(['post.id', 'user', 'post.url_slug', 'post.updated_at', 'post.released_at'])
-      .leftJoin('post.user', 'user')
-      .where('is_temp = false')
-      .andWhere('is_private = false')
-      .offset((page - 1) * 2500)
-      .limit(2500)
-      .orderBy('released_at', 'ASC')
-      .getMany();
-
-    const links: SitemapLink[] = posts.map(post => ({
-      location: `https://velog.io/@${post.user.username}/${encodeURI(post.url_slug)}`,
-      // TODO: implement release_updated_at
-      // lastmod: new Date(post.updated_at).toISOString(),
-      priority: 0.5,
-      changefreq: 'weekly',
-    }));
-    ctx.body = generateSitemap(links);
-  } catch (e) {
-    console.log(e);
-  }
+  ctx.status = 404;
+  return null;
+  // ctx.set('Content-Type', 'text/xml');
+  // const page = parseInt(ctx.params.page, 10);
+  // // ctx.body = { firstDay, lastDay };
+  // try {
+  //   const posts = await getRepository(Post)
+  //     .createQueryBuilder('post')
+  //     .select(['post.id', 'user', 'post.url_slug', 'post.updated_at', 'post.released_at'])
+  //     .leftJoin('post.user', 'user')
+  //     .where('is_temp = false')
+  //     .andWhere('is_private = false')
+  //     .offset((page - 1) * 2500)
+  //     .limit(2500)
+  //     .orderBy('released_at', 'ASC')
+  //     .getMany();
+  //   const links: SitemapLink[] = posts.map(post => ({
+  //     location: `https://velog.io/@${post.user.username}/${encodeURI(post.url_slug)}`,
+  //     // TODO: implement release_updated_at
+  //     // lastmod: new Date(post.updated_at).toISOString(),
+  //     priority: 0.5,
+  //     changefreq: 'weekly',
+  //   }));
+  //   ctx.body = generateSitemap(links);
+  // } catch (e) {
+  //   console.log(e);
+  // }
 };
